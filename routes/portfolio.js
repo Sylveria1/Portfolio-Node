919dfc506bcc4d9667fd152d90dcf250822cc43d
Trym <Ferdinand269@gmail.com>
2024-01-19 22:08:49 +0100

    Add POST handler for portfolio

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,12 +1,48 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
-const fs = require("fs")
-const path = require("path")
+const fs = require("fs");
+const path = require("path");
 
-/* GET home page. */
+// Download image to the server
+var download = function(url, filename, callback) {
+  request.head(url, function(err, res, body) {
+    if (err) {
+      console.error('Failed to download image:', err);
+      return callback(err);
+    }
+    var filePath = path.resolve(__dirname, '../data/img/' + filename);
+    request(url).pipe(fs.createWriteStream(filePath)).on('close', () => {
+      console.log('Download complete for:', filename);
+      callback(null); // Success
+    }).on('error', (error) => {
+      console.error('Error during download:', error);
+      callback(error); // Error during download
+    });
+  });
+};
+
 router.get('/', function(req, res, next) {
   let data = fs.readFileSync(path.resolve(__dirname, "../data/portfolio.json"));
-  res.render('portfolio', { cakes: JSON.parse(data)});
+  res.render('portfolio', { cakes: JSON.parse(data) });
+});
+
+router.post('/', express.json(), function(req, res) {
+  let rawData = fs.readFileSync(path.resolve(__dirname, "../data/portfolio.json"));
+  let portfoliosArray = JSON.parse(rawData);
+
+  if (!portfoliosArray.some(x => x.name === req.body.name)) {
+    download(req.body.url, req.body.name, function(error) {
+      if (error) {
+        return res.status(500).send('Failed to download image');
+      }
+      portfoliosArray.push(req.body);
+      fs.writeFileSync(path.resolve(__dirname, "../data/portfolio.json"), JSON.stringify(portfoliosArray));
+      res.status(200).send('Image added to portfolio');
+    });
+  } else {
+    res.status(400).send('Image already exists');
+  }
 });
 
 module.exports = router;
