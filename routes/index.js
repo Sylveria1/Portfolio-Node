2230d9a5d67080fbd69d794c5732567bbde566a3
Trym <Ferdinand269@gmail.com>
2024-01-19 12:56:36 +0100

    Refractor recommendations

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,20 @@ const path = require("path")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let data = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
-  res.render('index', { title: 'Express' , array: JSON.parse(data)});
+  // Read introductionArray.json
+  let introData = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"), "utf8");
+  let introArray = JSON.parse(introData);
+
+  // Read recommendations.json
+  let recommendationsData = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"), "utf8");
+  let recommendations = JSON.parse(recommendationsData);
+
+  // Pass both data sets to the view
+  res.render('index', { 
+    title: 'Express',
+    array: introArray,
+    recommendations: recommendations
+  });
 });
 
 module.exports = router;
